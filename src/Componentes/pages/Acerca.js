0500933885edb153e31fce3fbffcadee39c3c56b
karina-M <karina103mendez@gmail.com>
2021-12-15 21:38:55 -0600

    acerca
    :(:

--- src/Componentes/pages/Acerca.js
+++ src/Componentes/pages/Acerca.js
@@ -5,20 +5,19 @@ function Acerca()
     return(
         
         <form class="caja" method="post">
-            <center> 
-        <h1>Acerca de nosotros</h1>
+      
+        <h1 className="titulo"> Acerca de nosotros</h1> 
 
         <h2>Misión</h2>
 
-        <p>Somos una página dedicada ayudar a la sociedad con los típicos problemas que a quejan a las comunidades, esto lo hacemos como 
-            intermediario entre el ciudadano y el gobierno, con el fin de que nuestras comunidades mejoren para el bien de todos. </p>
+        <p>Somos una página dedicada ayudar a la sociedad con los típicos problemas que a quejan a las comunidades, 
+        esto lo hacemos como intermediario entre el ciudadano y el gobierno, con el fin de que nuestras comunidades mejoren para el bien de todos. </p>
 
         <h2>Visión </h2>
 
-        <p>Queremos ser una página web reconocida a nivel nacional, para poder ayudar a todas las comunidades de México, y así lograr una 
-            comunidad más tranquila y con mejores condiciones. </p>
+        <p>Queremos ser una página web reconocida a nivel nacional, para poder ayudar a todas las comunidades 
+            de México, y así lograr una comunidad más tranquila y con mejores condiciones. </p>
 
-            </center>
             </form>
 
       
