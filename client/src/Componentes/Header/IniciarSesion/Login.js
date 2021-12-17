54995dc5708470f197012fbe5681fd01e285d036
Bryan Hernandez <androbryaan@gmail.com>
2021-12-17 00:19:05 -0600

    bd

--- client/src/Componentes/Header/IniciarSesion/Login.js
+++ client/src/Componentes/Header/IniciarSesion/Login.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
+// import Axios from 'axios'
+
+
 
 function Login()
 {
+
+/*
+    const [usernameReg, setUsername] = useState('');
+    const [passwordReg, setPassword] = useState('');
+
+    const Registro = ()  => {
+        Axios.post('https://localhost:3001/registro', 
+        {username: usernameReg, password: passwordReg,
+        }).then((Response) => {console.log(Response);
+        });
+    };
+*/
     return(
     <React.Fragment>
         <form class="caja" method="post">
             <h2>Inicia sesión</h2>
-            <p>Correo electronico</p>
+            <p>Nombre de Usuario</p>
             <input type="text" name="log" class="field"/>
             <p>Contraseña</p>
             <input type="password" class="field"/>  
--- client/src/Componentes/Registro/Registro.js
+++ client/src/Componentes/Registro/Registro.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Registro.css";
+import Axios from 'axios'
+
+
 
 function Login()
 {
+
+    const [usernameReg, setUsername] = useState('');
+    const [passwordReg, setPassword] = useState('');
+
+    const Registro = ()  => {
+        Axios.post('http://localhost:3001/registro', {
+         username: usernameReg, 
+         password: passwordReg,
+        }).then((Response) => {console.log(Response);
+        });
+    };
+
     return(
     <React.Fragment>
         <form class="caja1" method="post">
             <h2>Ingresa tus datos</h2>
             <p>Nombre(s)</p>
-            <input type="text" name="log" class="field"/>
-            <p>Apellidos</p>
-            <input type="text" name="log" class="field"/>
-            <p>Direccion</p>
-            <input type="text" name="log" class="field"/>
-            <input type="submit" class="boton" value="Registrarme"/>
+            <input type="text" onChange={(e)=>{setUsername(e.target.value)}} name="log" class="field"/>
+            <p>Contraseña</p>
+            <input type="text" onChange={(e)=>{setPassword(e.target.value)}} name="log" class="field"/>
+            <button onClick={Registro} className="boton2">Dar de alta</button>
         </form>
 
     </React.Fragment>
